bd244c849c1789a0b25c4675efa571af3d40291d
David DeWert <daviddewert@gmail.com>
2017-11-07 23:00:22 -0800

    fixed LookAt_rh_fast

--- renderers/ViewMatrix.h
+++ renderers/ViewMatrix.h
@@ -5,21 +5,36 @@
 #include <Eigen/Geometry>
 
 
-using vec3f = Eigen::Vector3f;
-using vec4f = Eigen::Vector4f;
+using vec3f = Eigen::Matrix<float,3,1>;
+using vec4f = Eigen::Matrix<float,4,1>;
 using mat4f = Eigen::Matrix4f;
 using Map3f = Eigen::Map<const vec3f>;
 
-class TransformationMatrix : public Eigen::Matrix4f
+class TransformationMatrix 
 {
 public:
-	TransformationMatrix()
+	TransformationMatrix()  : _target{0,0,0}, _eye{0,0,3}, _up{0,0,1} 
 	{
-		Eigen::Matrix4f::block<4,1>(0,3) << 0,0,0,1;
+		_transform.block<4,1>(0,3) << 0,0,0,1;
 	}
-	void LookAt_rh(vec3f target);
-	void LookAt_rh(vec3f eye, vec3f target, vec3f up);
-	void LookAt_rh_fast(vec3f e, vec3f t, vec3f u);
+	void LookAt_rh(const vec3f& eye, const vec3f& target, const vec3f& up);
+	void LookAt_rh_fast(const vec3f& e, const vec3f& t, const  vec3f& u);
+	// void LookAt_rh_fast();
+
+
+	// void SetTarget(const vec3f target) { _target = target; }
+	// void setEye(const vec3f eye) { _eye = eye; }
+	// void setUp(const vec3f up) { _up = up; }
+
+	// const vec3f Target() { return _target; }
+	// const vec3f Eye() { return _eye; }
+	// const vec3f Up() { return _up; }
+	Eigen::Matrix4f _transform;
+
+private:
+	vec3f _target;
+	vec3f _eye;
+	vec3f _up;
 };	
 
 #endif
--- renderers/test.cpp
+++ renderers/test.cpp
@@ -7,19 +7,22 @@ main()
 	std::cout << "Testing Renderers" << std::endl;	
 
 	TransformationMatrix tm;
-	vec3f eye(0.0f,1.0f,0.0f);
+	vec3f eye;
 	vec3f target;
 	vec3f up;
 
-	eye.Random(); target.Random(); up.Random();
+	eye= vec3f::Random(); target = vec3f::Random(); up = vec3f::Random();
+	std::cout << "eye:\n" << eye << std::endl;
+	std::cout << "up:\n" << up << std::endl;
+	std::cout << "target:\n" << target << std::endl;
 
 	tm.LookAt_rh(eye, target, up);
-	std::cout << tm << std::endl;
 
-	tm.Random();
+	std::cout << tm._transform << std::endl;
 
 	tm.LookAt_rh_fast(eye, target, up);
 
-	std::cout << std::endl << tm << std::endl;
+
+	std::cout << std::endl << tm._transform << std::endl;
 	return 1;
 }
